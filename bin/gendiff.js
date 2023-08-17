5a5e64648e6dbdd0c3c7ef8027cfb0fdafb463ce
Oksana Fatkhulova <fato1284@gmail.com>
2023-08-17 18:12:20 +0300

    update bin/gendiff.js and package-lock.json and package.json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../index.js'
 
 program
  .description('Compares two configuration files and shows a difference.')
  .version('0.1')
  .option('-f, --format <type>', 'output format')
- .arguments('<filepath1>')
- .arguments('<filepath2>')
-;
+ .arguments('<filepath1> <filepath2>')
+ .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2))
+ });
 
 program.parse();
