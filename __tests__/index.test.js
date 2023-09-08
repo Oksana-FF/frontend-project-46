56bccdb0946924b37b0d8b7d6c8fd28032c8e90c
Oksana Fatkhulova <fato1284@gmail.com>
2023-09-08 14:15:07 +0300

    update __tests__/index.test.js and file1.json and file2.json

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,14 @@
 import genDiff from '../src/index.js';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('genDiff', () => {
-    expect(genDiff().toEqual());
+    const current = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+    const result = fs.readFileSync(getFixturePath('result1.txt'), 'utf-8')
+    expect(current).toEqual(result);
 });
